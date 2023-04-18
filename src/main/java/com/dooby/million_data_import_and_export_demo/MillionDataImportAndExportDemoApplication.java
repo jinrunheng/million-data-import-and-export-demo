c03b0b0a6fc1b2b385a347d089e0b7b4c8d1be1d
jinrunheng <1175088275@qq.com>
2023-04-18 20:01:40 +0800

    init project

---
+++ src/main/java/com/dooby/million_data_import_and_export_demo/MillionDataImportAndExportDemoApplication.java
@@ -0,0 +1,13 @@
+package com.dooby.million_data_import_and_export_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MillionDataImportAndExportDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MillionDataImportAndExportDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dooby/million_data_import_and_export_demo/MillionDataImportAndExportDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dooby.million_data_import_and_export_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MillionDataImportAndExportDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
